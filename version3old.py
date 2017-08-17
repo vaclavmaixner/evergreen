868232f95edde579eb2fadb4ca9faf38276dea09
Vaclav Maixner <vaclavmaixnerj@gmail.com>
2017-08-17 18:26:56 +0200

    zaloha pred tim, nez to zrychlime a dodelame

--- version3old.py
+++ version3old.py
@@ -27,6 +27,7 @@ class egExportParser(xml.sax.handler.ContentHandler):
 			self.subfield=None
 		elif tag=="subfield":
 			self.subfield=attrs.get('code')
+			self.egSubfield=None
 		elif tag=="record": 
 			self.datafield=None
 			self.subfield=None
@@ -39,13 +40,17 @@ class egExportParser(xml.sax.handler.ContentHandler):
 		#	print("yay")
 
 	def characters(self,data):
+		#idData = Id(data, )
+		#egId = None
+
+		
+
 		if self.datafield=="035" and self.subfield=="a":
 			if "(CZ PrNK)" in data:
 				data = data.replace("(CZ PrNK)", "")
 			data = data.strip()
 
 			if data!="":
-				self.nkSubfield = data
 				#idData=Id(egId, data)
 
 				#if not nkIdsFromEg:
@@ -53,40 +58,35 @@ class egExportParser(xml.sax.handler.ContentHandler):
 				#elif idData!=nkIdsFromEg[-1]:
 				#	nkIdsFromEg.append(idData)
 
-			if data!="" and len(data)<=4:
-				#print data + "chyba vole"
-				pass
+				self.nkSubfield = data
 
+			if data!="" and len(data)<=4:
+				print(data, "chyba vole")
 
 		if self.datafield=="901" and self.subfield=="c":
 			if data!="":
 				self.egSubfield=data
-				#print self.egSubfield + "laaa"
-				#if self.counter<=100:
-				#	print self.egSubfield
 
-		if self.egSubfield!="" and self.nkSubfield!="":
-			idData=Id(self.egSubfield,self.nkSubfield)
-			#print idData.egId + " " + idData.nkId + " kontrola"
-			if len(nkIdsFromEg)>1:	
-				if idData.egId!=nkIdsFromEg[-1].egId and idData.nkId!=nkIdsFromEg[-1].nkId:
-					nkIdsFromEg.append(idData)
-					print "yay"
-			#print str(idData.egId) + " " + str(idData.nkId) + " oi"
 			
+		if self.egSubfield and self.nkSubfield:
+			idData=Id(self.egSubfield,self.nkSubfield)
+			nkIdsFromEg.append(idData)
 
-			#self.egSubfield=None
-			#self.nkSubfield=None
+			self.egSubfield=None
+			self.nkSubfield=None
 
 class nkExportParser(xml.sax.handler.ContentHandler):
 	def startDocument(self):
 		self.controlfield=None
 		self.record=None
+		self.recordData=None
+		self.isRecord=False
 		self.counter=0
+		print "yup"
 
 	def startElement(self,tag,attrs):
-		if tag=="control":
-			self.datafield=attrs.get('tag')
+		if tag=="controlfield":
+			self.controlfield=attrs.get('tag')
 		elif tag=="record": 
 			self.controlfield=None
 			self.counter+=1
@@ -95,18 +95,19 @@ class nkExportParser(xml.sax.handler.ContentHandler):
 				sys.stdout.flush()
 
 	def characters(self,data):
-		if self.counter <= 5000:	
-			code = "%s%s"%(self.datafield,self.subfield)
-			if self.controlfield=="001":
-				if data in nkIdsFromEg:
-					#print(data, "success")
-					pass
-
-
+		if self.controlfield=="001":
+			for i in nkIdsFromEg:
+				pass
+				if i.nkId==data:
+					outXmlFile.write(i.egId + "	" + "success")
 
 
 nkIdsFromEg = []
 
+pathOut = "biblio_output.xml"
+
+outXmlFile = codecs.open(pathOut,"w","utf-8")
+outXmlFile.write('<collection xmlns="http://www.loc.gov/MARC21/slim" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.loc.gov/MARC21/slim http://www.loc.gov/standards/marcxml/schema/MARC21slim.xsd">')
 
 
 egExportParser = egExportParser()
@@ -114,12 +115,16 @@ parser = xml.sax.parse("EG_auth_UTF-8.xml", egExportParser)
 print("Projetych zaznamu z EG: ", len(nkIdsFromEg))
 
 nkExportParser = nkExportParser()
-parser = xml.sax.parse("aut_test.xml", egExportParser)
+parser = xml.sax.parse("../autority_zaklad/aut_exp.xml", nkExportParser)
 
 #print out the data
-for i in nkIdsFromEg:
-	print str(i.egId ) + " " + i.nkId
-	
+#for i in nkIdsFromEg:
+#	print i.egId, i.nkId
+
+
+outXmlFile.write('</collection> uspech2')
+outXmlFile.close()
+print "konec vole"
 
 #z EG aut vezme NK cisla a nasype je do pameti, pak se projede saxem
 #NK aut a ty, ktery jsou v pameti, napise spolu s id z EG
