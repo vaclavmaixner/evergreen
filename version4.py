c9fc35d6dd6ec37d10ca88a4f250dd2ebce6424f
Vaclav Maixner <vaclavmaixnerj@gmail.com>
2017-08-30 18:07:35 +0200

    finalni aktualizace

--- version4.py
+++ version4.py
@@ -8,13 +8,6 @@ import codecs
 import argparse
 import re
 
-textXml=""
-
-class Id():
-	def __init__(self, egId, nkId):
-		self.egId=egId
-		self.nkId=nkId
-
 class egExportParser(xml.sax.handler.ContentHandler):
 	def startDocument(self):
 		self.datafield=None
@@ -23,7 +16,7 @@ class egExportParser(xml.sax.handler.ContentHandler):
 		self.egSubfield=None        
 		self.nkSubfield=None        
 		self.counter=0
-		
+		self.wrongData=False
 
 	def startElement(self,tag,attrs):
 		if tag=="datafield":
@@ -41,22 +34,26 @@ class egExportParser(xml.sax.handler.ContentHandler):
 				sys.stdout.flush()
 
 	def characters(self,data):
+		data = data.strip()
 		if self.datafield=="035" and self.subfield=="a":
 			if "(CZ PrNK)" in data:
 				data = data.replace("(CZ PrNK)", "")
-			data = data.strip()
 
 			if data!="":
 				self.nkSubfield = data
 
 			if data!="" and len(data)<=4:
-				print data, "chyba vole"
+				#print data, " Nasel jsem tu chybu >:[ ", self.counter
+				#self.wrongData=True
+				pass
 
 		if self.datafield=="901" and self.subfield=="c":
 			if data!="":
 				self.egSubfield=data
+			if self.wrongData:
+				print data, "chybnej zaznam"
+				self.wrongData=False
 
-			
 		if self.egSubfield and self.nkSubfield:
 			nkIdsFromEg[self.nkSubfield]=self.egSubfield
 
@@ -119,45 +116,50 @@ class nkExportParser(xml.sax.handler.ContentHandler):
 			self.txtXml+='</leader>'
 		elif tag=="record": 
 			self.txtXml+='</record>'
-			self.txtXml+='\n'
+			#self.txtXml+='\n'
 			if self.writeToOutput:
+				self.txtXml=self.txtXml
 				outXmlFile.write(self.txtXml)
 			self.writeToOutput=False
 			self.txtXml=""
 			
 	def characters(self,data):
-		self.txtXml += data
+		self.txtXml += data.rstrip()
 		if self.controlfield=="001":
 			self.idIn = nkIdsFromEg.get(data)
 			if self.idIn!=None:
 				outXmlFile.write(nkIdsFromEg[data] + "	")
 				self.writeToOutput=True
 
-			
+
+
 
 nkIdsFromEg = {}
 
-pathOut = "biblio_output.xml"
+parser = argparse.ArgumentParser(description='Nazdarek')
+
+parser.add_argument("egFile",nargs='?',help="export XML file",default="EG_auth_UTF-8.xml") 
+parser.add_argument("nkFile",nargs='?',help="authority XML file",default="../autority_zaklad/aut_exp.xml")
+parser.add_argument("-o","--output",help="output file",default="biblio_output.txt")
+
+args = parser.parse_args()
+
+egFile=args.egFile
+nkFile=args.nkFile
+pathOut = args.output
 
 outXmlFile = codecs.open(pathOut,"w","utf-8")
-outXmlFile.write('<collection xmlns="http://www.loc.gov/MARC21/slim" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.loc.gov/MARC21/slim http://www.loc.gov/standards/marcxml/schema/MARC21slim.xsd">')
-
 
 egExportParser = egExportParser()
-parser = xml.sax.parse("EG_auth_UTF-8.xml", egExportParser)
+parser = xml.sax.parse(egFile, egExportParser)
 print("Projetych zaznamu z EG: ", len(nkIdsFromEg))
 
 nkExportParser = nkExportParser()
-parser = xml.sax.parse("../autority_zaklad/aut_exp.xml", nkExportParser)
+parser = xml.sax.parse(nkFile, nkExportParser)
 
-#print out the data
-#for i in nkIdsFromEg:
-#	print i.egId, i.nkId
-
-
-outXmlFile.write('</collection> uspech2')
 outXmlFile.close()
-print "konec vole"
-
-#z EG aut vezme NK cisla a nasype je do pameti, pak se projede saxem
-#NK aut a ty, ktery jsou v pameti, napise spolu s id z EG
+print "Konec :]"
+os.system('play --no-show-progress --null --channels 1 synth %s sine %f' % ( 0.2, 1000))
+os.system('play --no-show-progress --null --channels 1 synth %s sine %f' % ( 0.2, 1500))
+os.system('play --no-show-progress --null --channels 1 synth %s sine %f' % ( 0.2, 2500))
+os.system('play --no-show-progress --null --channels 1 synth %s sine %f' % ( 0.4, 1000))
